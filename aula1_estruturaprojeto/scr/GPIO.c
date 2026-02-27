8d7d014cf233dff80f7adc521cd54ad6f73b5053
Roberto Paravizi <roberto.j2003@aluno.ifsc.edu.br>
2026-02-27 16:42:21 -0300

    alteração cod led

--- aula1_estruturaprojeto/scr/GPIO.c
+++ aula1_estruturaprojeto/scr/GPIO.c
@@ -1,10 +1,14 @@
+#define F_CPU 16000000
 #include "GPIO.h"
 #include <xc.h>
+#include "util/delay.h"
 
 void GPIO_initialize(){
 	DDRB = (1<<DDB0);    //configura o pino PB0 como saida
 }
 void GPIO_blinkLed(){
 	PORTB |= (1<<PORTB0);  //aciona o pino
+	_delay_ms(300);
 	PORTB &= ~(1<<PORTB0); 
+	_delay_ms(700);
 }
