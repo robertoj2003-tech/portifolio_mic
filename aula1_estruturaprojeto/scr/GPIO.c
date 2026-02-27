4173a8008d4691e42030b0ca7a6f0f7e221f3c26
Roberto Paravizi <roberto.j2003@aluno.ifsc.edu.br>
2026-02-27 16:36:02 -0300

    Corrigida lógica do led

--- aula1_estruturaprojeto/scr/GPIO.c
+++ aula1_estruturaprojeto/scr/GPIO.c
@@ -5,5 +5,6 @@ void GPIO_initialize(){
 	DDRB = (1<<DDB0);    //configura o pino PB0 como saida
 }
 void GPIO_blinkLed(){
-	PORTB = (1<<PORTB0);  //aciona o pino
+	PORTB |= (1<<PORTB0);  //aciona o pino
+	PORTB &= ~(1<<PORTB0); 
 }
